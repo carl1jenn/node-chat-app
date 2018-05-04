107509230efb68d16a4a9661b483a1c74e0e37d1
Carl Eastman <carl1jenn@gmail.com>
2018-05-04 07:20:46 -0500

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'jenn@example.com',
-    text: 'hey this is a new message'
-  });
-
 });
 
 socket.on('disconnect', function () {
